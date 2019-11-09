67084a25f0f3c802550933b3262679c9b21e5d6f
ghrthrth <50299852+ghrthrth@users.noreply.github.com>
2019-11-09 09:47:48 +0300

    Update Responce.java

--- GeocoderService/core/src/main/java/com/geocoderservice/core/model/Responce.java
+++ GeocoderService/core/src/main/java/com/geocoderservice/core/model/Responce.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.io.Serializable;
 
 import javax.persistence.*;
 
@@ -13,12 +14,50 @@ import javax.persistence.*;
 @Table(name="result")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Otvet {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Responce {
+    @Entity
+public class Responce implements Serializable {
 
-    private String dolgota;
-    private String shirota;
+    private Long id;
     private String adress;
+    private String longitude;
+    private String latitude;
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false)
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Column
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String phone) {
+        this.longitude = phone;
+    }
+
+    @Column
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String phone) {
+        this.latitude = latitude;
+    }
+
 }
